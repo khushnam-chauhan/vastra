5b3a0c66629e03f500cba820cc0eeeda0d00c2c4
khushnam <chauhankhushnam@gmail.com>
2024-03-28 11:25:47 +0530

    popular css

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from '../Component/hero/Hero'
 import Popular from '../Component/popular/Popular'
-
+import Offers from '../Component/offers/Offers'
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular />
+      <Offers />
     </div>
   )
 }
