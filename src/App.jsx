bcecf914c2759a30c376917f04aec2bb9fe751c8
khushnam <chauhankhushnam@gmail.com>
2024-03-28 11:08:31 +0530

    initials

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './Component/Navbar/Navbar'
+import {BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import Shop from './Pages/Shop'
+import LoginSignup from './Pages/LoginSignup'
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/men" element={<ShopCategory category='men'/>}/>
+        <Route path="/women" element={<ShopCategory category='women'/>}/>
+        <Route path="/kids" element={<ShopCategory category='kids'/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':id' element={<Product/>}/>
+        </Route>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<LoginSignup/>} />
+        
+
+      </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Component/hero/Hero'
+import Popular from '../Component/popular/Popular'
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero/>
+      <Popular />
+    </div>
+  )
+}
+
+export default Shop
